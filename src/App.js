afd77768e54735a4dd52ddfae84a1781773d03ac
_pratiksha_bhagat_ <139431858+Pratikshabh2810@users.noreply.github.com>
2024-08-02 11:54:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import Sidebar from "../src/components/sidebar/Sidebar";
+
+import Notecontainer from "./components/Notecontainer/Notecontainer";
+
+
+import "./App.css";
+
+function App() {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes-app")) || []
+  );
+
+  const addNote = (color) => {
+    const tempNotes = [...notes];
+
+    tempNotes.push({
+      id: Date.now() + "" + Math.floor(Math.random() * 78),
+      text: "",
+      time: Date.now(),
+      color,
+    });
+    setNotes(tempNotes);
+  };
+
+  const deleteNote = (id) => {
+    const tempNotes = [...notes];
+
+    const index = tempNotes.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    tempNotes.splice(index, 1);
+    setNotes(tempNotes);
+  };
+
+  const updateText = (text, id) => {
+    const tempNotes = [...notes];
+
+    const index = tempNotes.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    tempNotes[index].text = text;
+    setNotes(tempNotes);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("notes-app", JSON.stringify(notes));
+  }, [notes]);
+
+  return (
+    <div className="App">
+      <Sidebar addNote={addNote} />
+      <Notecontainer
+        notes={notes}
+        deleteNote={deleteNote}
+        updateText={updateText}
+      />
+    </div>
+  );
+}
+
+export default App;
